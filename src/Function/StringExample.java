dfa5343cdc287340b313e415216c1c500b069d8a
josephdev243 <josephdev243@gmail.com>
2025-03-23 06:44:35 +0300

    Initial commit

---
+++ src/Function/StringExample.java
@@ -0,0 +1,14 @@
+package Function;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+
+        String greeting = "How are you?";
+        return greeting;
+    }
+}
